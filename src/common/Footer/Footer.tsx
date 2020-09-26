aeacec52b85e4f95d7c4fd1213a60c8122442754
Cody Hennighan <cody@Codys-MacBook-Air.local>
2020-09-26 20:26:18 +0800

    Footer

---
+++ src/common/Footer/Footer.tsx
@@ -0,0 +1,49 @@
+import { faCocktail, faLemon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import styled from "styled-components";
+import { COLOURS } from "../COLOURS";
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  height: 42px;
+  border-top: 1px solid ${COLOURS.Grey};
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const FooterButton = styled.div`
+  height: 40px;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  cursor: pointer;
+  flex: 1 1 auto;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  flex: 1 1 auto;
+`;
+
+const FooterButtonText = styled.span`
+  flex: 1 1 auto;
+  font-size: 12px;
+`;
+
+export const Footer = () => (
+  <Container>
+    <FooterButton>
+      <StyledIcon icon={faLemon} />
+      <FooterButtonText>Ingredients</FooterButtonText>
+    </FooterButton>
+    <FooterButton>
+      <StyledIcon icon={faCocktail} />
+      <FooterButtonText>Cocktails</FooterButtonText>
+    </FooterButton>
+  </Container>
+);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { CardList } from './Card/CardList';
 import styled from 'styled-components';
+import { Footer } from './common/Footer/Footer';
 
 const RootContainer = styled.div`
   height: 100vh;
@@ -12,6 +13,7 @@ const RootContainer = styled.div`
 ReactDOM.render(
   <React.StrictMode>
     <RootContainer>
+      <Footer />
       <CardList />
     </RootContainer>
   </React.StrictMode>,
